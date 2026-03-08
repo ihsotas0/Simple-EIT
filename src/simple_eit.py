2793b68cb07de474bbfafa7dbf4231aac85033ac
Jonah Spector <jonah.m.spector@proton.me>
2026-03-08 13:11:42 -0600

    Finished MUX switching and voltage gathering code, with new softmax normalized display

--- src/simple_eit.py
+++ src/simple_eit.py
@@ -31,11 +31,7 @@ class SimpleEIT:
         self.fig, self.ax = plt.subplots()
 
         self.im = self.ax.imshow(
-            self.display,
-            cmap="binary",
-            origin="lower",
-            vmin=0,
-            vmax=1
+            self.display, cmap="binary", origin="lower", vmin=0, vmax=1
         )
 
         self.ax.set_title("Location of Object of Higher Resistivity")
@@ -45,41 +41,118 @@ class SimpleEIT:
         self.ax.set_xticklabels(["Left (D)", "Right (B)"])
         self.ax.set_yticklabels(["Bottom (C)", "Top (A)"])
 
+    # def get_voltages(self):
+
+    #     v_return = np.zero(6)
+
+    #     # Configuration 2 (S is A-C)
+    #     self.set_mux(0, 0)
+    #     v_return[1] = self.dm.get_voltage()
+
+    #     # Configuration 3 (S is A-D)
+    #     self.set_mux(0, 2)
+    #     v_return[2] = self.dm.get_voltage()
+
+    #     # Configuration 5 (S is B-D)
+    #     self.set_mux(1, 2)
+    #     v_return[4] = self.dm.get_voltage()
+
+    #     # Configuration 4 (S is B-C)
+    #     self.set_mux(1, 0)
+    #     v_return[3] = self.dm.get_voltage()
+
+    #     # Configuration 6 (S is C-D)
+    #     self.set_mux(2, 1)
+    #     v_return[5] = self.dm.get_voltage()
+
+    #     # Configuration 1 (S is A-B)
+    #     self.set_mux(3, 3)
+    #     v_return[0] = self.dm.get_voltage()
+
+    #     return v_return
+
     def get_voltages(self):
+        v_return = np.zero(6)
+        # (selection for MUX 1, selection for MUX 2, voltage measurement index)
+        configs = [(0, 0, 1), (0, 2, 2), (1, 2, 4), (1, 0, 3), (2, 1, 5), (3, 3, 0)]
 
-        self.set_mux(0, 0)
-        
-        return np.random.rand(6)
+        for mux1, mux2, index in configs:
+            self.set_mux(mux1, mux2)
+            v_return[index] = self.dm.get_voltage()
 
-    def set_mux(self, c1, c2)
-        """ Controls selections for MUX 1 and MUX 2 using GPIO"""
-        match c1:
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-        match c2:
-            case 0:
-            case 1:
-            case 2:
-            case 3:
+        return v_return
+
+    # def set_mux(self, c1, c2)
+    #     """ Controls selections for MUX 1 and MUX 2 using GPIO"""
+    #     match c1:
+    #         case 0:
+    #             self.mux1[0].off()
+    #             self.mux1[1].off()
+    #         case 1:
+    #             self.mux1[0].off()
+    #             self.mux1[1].on()
+    #         case 2:
+    #             self.mux1[0].on()
+    #             self.mux1[1].off()
+    #         case 3:
+    #             self.mux1[0].on()
+    #             self.mux1[1].on()
+    #     match c2:
+    #         case 0:
+    #             self.mux2[0].off()
+    #             self.mux2[1].off()
+    #         case 1:
+    #             self.mux2[0].off()
+    #             self.mux2[1].on()
+    #         case 2:
+    #             self.mux2[0].on()
+    #             self.mux2[1].off()
+    #         case 3:
+    #             self.mux2[0].on()
+    #             self.mux2[1].on()
+
+    def set_mux(self, c1, c2):
+        """Controls selections for MUX 1 and MUX 2 using GPIO"""
+
+        def set_mux_state(mux, state):
+            mux[0].off() if state & 0b01 == 0 else mux[0].on()
+            mux[1].off() if state & 0b10 == 0 else mux[1].on()
+
+        set_mux_state(self.mux1, c1)
+        set_mux_state(self.mux2, c2)
 
     def update(self, frame):
 
         self.display[:] = 0
-        voltages = self.get_voltages()
+        v = self.get_voltages()  # Get the measured voltages
 
-        # Decision tree
-        if voltages[1] < voltages[4]:
-            if voltages[0] > voltages[5]:
-                self.display[0, 1] = 1
-            else:
-                self.display[1, 0] = 1
-        else:
-            if voltages[2] > voltages[3]:
-                self.display[0, 0] = 1
-            else:
-                self.display[1, 1] = 1
+        # Compute conditionals numerically (+) for yes (-) for no. Magnitude of
+        # conditional shows the "confidence" that the OHR is in that quadrant
+        v_raw = np.array(
+            (v[1] - v[4]) * (v[2] - v[3]),  # self.display[0, 0]
+            (v[4] - v[1]) * (v[0] - v[5]),  # self.display[0, 1]
+            (v[4] - v[1]) * (v[5] - v[0]),  # self.display[1, 0]
+            (v[1] - v[4]) * (v[3] - v[2]),
+        )  # self.display[1, 1]
+
+        # The above code does this "Decision tree" numerically
+
+        # # Decision tree
+        # if voltages[1] < voltages[4]:
+        #     if voltages[0] > voltages[5]:
+        #         self.display[0, 1] = 1
+        #     else:
+        #         self.display[1, 0] = 1
+        # else:
+        #     if voltages[2] > voltages[3]:
+        #         self.display[0, 0] = 1
+        #     else:
+        #         self.display[1, 1] = 1
+
+        # Normalize conditionals to represent a probabilty distribution
+        v_norm = softmax(v_raw)
+
+        self.display = v_norm.reshape(2, 2)
 
         self.im.set_data(self.display)
 
@@ -90,12 +163,16 @@ class SimpleEIT:
         self.ani = FuncAnimation(
             self.fig,
             self.update,
-            interval=20,
+            interval=100,  # IMPORTANT: Update period in ms
             blit=True,
-            cache_frame_data=False
+            cache_frame_data=False,
         )
 
         plt.show()
+
+
+def softmax(x):
+    return np.exp(x - max(x)) / sum(np.exp(x - max(x)))
 
 
 if __name__ == "__main__":
