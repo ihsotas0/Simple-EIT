586a8b9ab7bd0b95942f13a8a15432417ffc9232
Jonah Spector <jonah.m.spector@proton.me>
2026-03-27 15:46:07 -0600

    BUG FIX: The configuration indexes were flipped, other cleanup

--- src/eit.py
+++ src/eit.py
@@ -2,50 +2,66 @@ import datetime
 import time
 
 import numpy as np
-#from gpiozero import LED
+from gpiozero import LED
 
-#from device_manager import DeviceManager
+from device_manager import DeviceManager
 
 
 class SimpleEIT:
 
     def __init__(self):
 
-        # # GPIO controllers for multiplexers
-        # # MUX1 controls S+ and V+ (A0, A1)
-        # self.mux1 = (LED(26), LED(19))
+        # GPIO controllers for multiplexers
+        # MUX1 controls S+ and V+ (A0, A1)
+        self.mux1 = (LED(26), LED(19))
 
-        # # MUX2 controls S- and V- (A0, A1)
-        # self.mux2 = (LED(13), LED(6))
+        # MUX2 controls S- and V- (A0, A1)
+        self.mux2 = (LED(13), LED(6))
 
-        # # Device manager
-        # self.dm = DeviceManager()
+        # Turns MUXs off before switching and on again
+        self.mux_toggle = (LED(5), LED(16))
 
-        # # Configure devices
-        # self.dm.set_voltmeter()
-        # self.dm.set_wavegen()
+        # Device manager
+        self.dm = DeviceManager()
 
-        self.test = 0
+        # Configure devices
+        self.dm.set_voltmeter()
+        self.dm.initialize_voltmeter()
+        self.dm.set_wavegen()
+
+        # self.test = 0
 
     def get_voltages(self):
-        # v_return = np.zeros(6)
+        v_return = np.zeros(6)
 
-        # # (selection for MUX 1, selection for MUX 2, voltage measurement index)
-        # configs = [(0, 0, 1), (0, 2, 2), (1, 2, 4), (1, 0, 3), (2, 1, 5), (3, 3, 0)]
+        # (selection for MUX 1, selection for MUX 2, voltage measurement index)
+        configs = [(0, 0, 4), (0, 2, 3), (1, 2, 1), (1, 0, 2), (2, 1, 0), (3, 3, 5)]
 
-        # for s1, s2, index in configs:
-        #     set_mux_state(self.mux1, s1)  # Apply state to mux1
-        #     set_mux_state(self.mux2, s2)  # Apply state to mux2
-        #     print(f"Switched MUX 1: {s1}, MUX 2: {s2}, Config: {index}")
-        #     v_return[index] = self.dm.get_voltage()
+        for s1, s2, index in configs:
+            
+            # Start MUX switch (ground measurement leads)
+            mux_toggle[0].off()
+            mux_toggle[1].off()
 
-        # ct = datetime.datetime.now()
+            # Switch MUX state
+            set_mux_state(self.mux1, s1)  # Apply state to mux1
+            set_mux_state(self.mux2, s2)  # Apply state to mux2
+            print(f"Switched MUX 1: {s1}, MUX 2: {s2}, Config: {index}")
+            
+            # End MUX switch (turn measurement leads on)
+            mux_toggle[0].on()
+            mux_toggle[1].on()
+            
+            # Get voltage, assign to index (V_AB, V_AC, V_AD, V_BC, V_BD, V_CD)
+            v_return[index] = self.dm.get_voltage()
 
-        # print(f"{ct}: Measured voltages: {v_return:6f}")
+        ct = datetime.datetime.now()
 
-        # return v_return
+        print(f"{ct}: Measured voltages: {v_return:6f}")
 
-        return np.random.uniform(-1, 1, 6)
+        return v_return
+
+        # return np.random.uniform(-1, 1, 6)
 
     def run(self):
 
@@ -68,6 +84,7 @@ class SimpleEIT:
 
         return v_norm.reshape(2, 2)
 
+
 def softmax(x):
     return np.exp(x - max(x)) / sum(np.exp(x - max(x)))
 
@@ -77,9 +94,7 @@ def negneg(x, y):
     return -(x * y) if x < 0 and y < 0 else x * y
 
 
-"""Helper functions"""
-
-
+# Helper functions
 
 def set_mux_state(mux, state):
     """Set mux GPIO states based on the 2-bit state."""
@@ -89,8 +104,7 @@ def set_mux_state(mux, state):
     mux[1].on() if state & 0b10 else mux[1].off()
 
 
-"""Testing"""
-
+# Testing
 if __name__ == "__main__":
     # For testing
     app = SimpleEIT()
