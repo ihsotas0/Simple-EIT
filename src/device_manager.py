67d98d7c87582b0b59921f2c9b62482c8656b6e2
Jonah Spector <jonah.m.spector@proton.me>
2026-04-19 20:40:51 -0600

    Quality of life

--- src/device_manager.py
+++ src/device_manager.py
@@ -173,7 +173,7 @@ class DeviceManager:
 # ========= Testing =========
 
 def basic_device_test():
-    with DeviceManager(wavegen_idn=input('Wavegen: '), scope_idn=input('Scope: ')) as dm:
+    with DeviceManager(wavegen_idn='EDU33212A', scope_idn='EDU-X') as dm:
 
         for i in range(25):
             voltage = dm.get_voltage()
